6c1e095902db2c43184c08158e54b5bb6a06a351
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-12-29 13:45:24 +0300

    + Feat : create game manager script

---
+++ Just a Normal Flappy Bird Game/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameManager instance;
+    public static GameManager Instance => instance ?? (instance = FindObjectOfType<GameManager>());
+    private void Awake()
+    {
+
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+    }
+    #endregion
+
+    [SerializeField] private PipeController pipeController;
+
+    public void GameOver()
+    {
+        pipeController.isGameOver = true;
+    }
+}
